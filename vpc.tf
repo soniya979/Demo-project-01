ef7cbd6fadfb2716d31241f5f827777ba3701f63
soniya979 <soniyapatel979@gmail.com>
2022-06-17 22:20:47 +0100

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -27,6 +27,10 @@ vpc_id = aws_vpc.demo-vpc.id
 cidr_block = "10.0.0.0/24"
 availability_zone = "eu-west-2a"
 map_public_ip_on_launch = "true"
+
+tags = {
+    Name = "rds-db-pub-subnet01"
+  }
 }
 
 resource "aws_subnet" "rds-db-pub-subnet02" {
@@ -34,6 +38,10 @@ vpc_id = aws_vpc.demo-vpc.id
 cidr_block = "10.0.1.0/24"
 availability_zone = "eu-west-2b"
 map_public_ip_on_launch = "true"
+ 
+tags = {
+    Name = "rds-db-pub-subnet02"
+  }
 }
 
 #create redshift cluster subnets in different AZs
