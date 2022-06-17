1e041ebdd45706aeb7d4abb79b83420a500fc325
soniya979 <soniyapatel979@gmail.com>
2022-06-17 23:09:00 +0100

    Update rds-db.tf

--- rds-db.tf
+++ rds-db.tf
@@ -16,7 +16,7 @@ skip_final_snapshot  = true
 db_subnet_group_name = aws_db_subnet_group.rds-db-subnet-group.id
 availability_zone  = "eu-west-2a"
 deletion_protection = true
-vpc_security_group_ids = [aws_security_group.demo-rds-sg02.id]
+vpc_security_group_ids = [aws_security_group.demo-rds-sg02.id, aws_security_group.demo-rds-sg01.id]
 
 tags = {
   Name = "demordsDB01"
