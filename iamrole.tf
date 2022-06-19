c16a2fbc59c4433189a74e306f0214f0f96bdcd2
soniya979 <soniyapatel979@gmail.com>
2022-06-19 13:18:19 +0100

    Update iamrole.tf

--- iamrole.tf
+++ iamrole.tf
@@ -89,7 +89,7 @@ resource "aws_iam_role_policy" "s3-full-access-policy" {
             Action = ["s3:*"]
             Resource = "*"
         },
-]
+     ]
 })
 
 }
@@ -116,4 +116,92 @@ assume_role_policy = jsonencode({
 tags = {
     tag-key = "demo-redshift-role"
   }
+}
+========================EMR-Role===============================
+  
+# IAM role for EMR Service
+
+resource "aws_iam_role" "emr-role" {
+  name = "emr-role"
+
+  assume_role_policy = jsonencode({
+  Version = "2008-10-17"
+  Statement = [
+    {
+       Action = "sts:AssumeRole"
+       Effect = "Allow"
+       Sid = ""
+       Principal = {
+       Service = "elasticmapreduce.amazonaws.com" 
+      }
+    },
+  ]
+})
+
+resource "aws_iam_role_policy" "emr-role-policy" {
+  name = "emr-role-policy"
+  role = aws_iam_role.emr-role.id
+
+  policy = jsonencode({
+     Version = "2012-10-17",
+     Statement = [{
+       Effect = "Allow",
+       Resource = "*",
+        Action = [
+            "ec2:AuthorizeSecurityGroupEgress",
+            "ec2:AuthorizeSecurityGroupIngress",
+            "ec2:CancelSpotInstanceRequests",
+            "ec2:CreateNetworkInterface",
+            "ec2:CreateSecurityGroup",
+            "ec2:CreateTags",
+            "ec2:DeleteNetworkInterface",
+            "ec2:DeleteSecurityGroup",
+            "ec2:DeleteTags",
+            "ec2:DescribeAvailabilityZones",
+            "ec2:DescribeAccountAttributes",
+            "ec2:DescribeDhcpOptions",
+            "ec2:DescribeInstanceStatus",
+            "ec2:DescribeInstances",
+            "ec2:DescribeKeyPairs",
+            "ec2:DescribeNetworkAcls",
+            "ec2:DescribeNetworkInterfaces",
+            "ec2:DescribePrefixLists",
+            "ec2:DescribeRouteTables",
+            "ec2:DescribeSecurityGroups",
+            "ec2:DescribeSpotInstanceRequests",
+            "ec2:DescribeSpotPriceHistory",
+            "ec2:DescribeSubnets",
+            "ec2:DescribeVpcAttribute",
+            "ec2:DescribeVpcEndpoints",
+            "ec2:DescribeVpcEndpointServices",
+            "ec2:DescribeVpcs",
+            "ec2:DetachNetworkInterface",
+            "ec2:ModifyImageAttribute",
+            "ec2:ModifyInstanceAttribute",
+            "ec2:RequestSpotInstances",
+            "ec2:RevokeSecurityGroupEgress",
+            "ec2:RunInstances",
+            "ec2:TerminateInstances",
+            "ec2:DeleteVolume",
+            "ec2:DescribeVolumeStatus",
+            "ec2:DescribeVolumes",
+            "ec2:DetachVolume",
+            "iam:GetRole",
+            "iam:GetRolePolicy",
+            "iam:ListInstanceProfiles",
+            "iam:ListRolePolicies",
+            "iam:PassRole",
+            "s3:CreateBucket",
+            "s3:Get*",
+            "s3:List*",
+            "sdb:BatchPutAttributes",
+            "sdb:Select",
+            "sqs:CreateQueue",
+            "sqs:Delete*",
+            "sqs:GetQueue*",
+            "sqs:PurgeQueue",
+            "sqs:ReceiveMessage"
+        ]
+    }]
+})
 }
