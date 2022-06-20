0261798a35f2b27174649fc6bd9851a79dbdce83
soniya979 <soniyapatel979@gmail.com>
2022-06-20 09:18:44 +0100

    Update emr-cluster.tf

--- emr-cluster.tf
+++ emr-cluster.tf
@@ -2,16 +2,23 @@ resource "aws_emr_cluster" "demo-emr-cluster" {
   name          = "demo-emr-cluster"
   release_label = "emr-5.36.0"
   applications  = ["Spark"]
+  service_role = "${aws_iam_role.EMR_DefaultRole.id}"
+	log_uri = "s3://${aws_s3_bucket.s3_bucket.bucket}/logs/"
+	tags = {
+		name = "EMR cluster created via Terraform"
+	}
 
   ec2_attributes {
-    key_name 			                    = "emrec2key"
+    instance_profile                  = aws_iam_instance_profile.demo-emr-instance-profile.arn
+    key_name 			                  = "emrec2key"
     subnet_id                         = aws_subnet.emr-subnet.id
     emr_managed_master_security_group = aws_security_group.emr-sg01.id
-    emr_managed_slave_security_group  = aws_security_group.emr-sg01.id
-    instance_profile                  = aws_iam_instance_profile.demo-emr-instance-profile.arn
+#   emr_managed_slave_security_group  = aws_security_group.emr-sg01.id
+    
   }
 
   master_instance_group {
+    name = "EMR Master EC2"
     instance_type = "m5.xlarge"
   }
 
